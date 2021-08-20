c259162de323098d3b2cbc668d5fec606c1bd0a0
sergei-p <sergeidev2494@gmail.com>
2021-08-19 21:28:22 -0700

    Created Header View

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './scss/style.scss';
 import NavBar from "./components/NavBar";
-// import Header from "./components/Header";
+import Header from "./components/Header";
 // import AboutMe from "./components/AboutMe";
 // import Projects from "./components/Projects";
 // import Contact from "./components/Contact";
@@ -10,9 +10,9 @@ function App() {
   return (
       <div>
       <NavBar/>
-      {/* <Header/>
-      <AboutMe/>
-      <Projects/>
+      <Header/>
+      {/* <AboutMe/> */}
+      {/*<Projects/>
       <Contact/>
       <Footer/> */}
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function Header() {
-
+  
+  return(
+    <header>
+      <div className="imageContainer">
+        <img src="https://raw.githubusercontent.com/chuckgroom/onepage-bio/master/img/profile.png" alt="My Image"/>
+      </div>
+      <div className="nameContainer">
+        <h2>Sergei Poliakov</h2>
+      </div>
+      <div className="titleContainer">
+        <h3>Software Engineer</h3>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
