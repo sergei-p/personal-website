d46ef1dd37cfb578820915a26c6f67f8b1bc1849
sergei-p <sergeidev2494@gmail.com>
2021-08-19 19:40:55 -0700

    New components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import './scss/style.scss';
+import NavBar from "./components/NavBar";
+// import Header from "./components/Header";
+// import AboutMe from "./components/AboutMe";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <p>My Site</p>
-    </>
+      <div>
+      <NavBar/>
+      {/* <Header/>
+      <AboutMe/>
+      <Projects/>
+      <Contact/>
+      <Footer/> */}
+    </div>
+    
+    
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+
+function NavBar() {
+	
+	return(
+    // <div>
+    //     <nav>
+    //     <h1 className="logo">My Site</h1>
+		// 		<ul>
+		// 			<li><SocialIcon url="https://www.linkedin.com/in/sergei-poliakov-6b2b52213/"/></li>
+		// 			<li><SocialIcon url="https://github.com/sergei-p"/></li>
+		// 		</ul>
+		// 		</nav>
+    // </div>
+		
+		<nav className="navbar">
+			{/* <div className="navContainer"> */}
+				<div className="nameContainer">
+					<h3>Sergei Poliakov</h3>
+				</div>
+				<div className="socialLinks">
+					<ul>
+						<li><SocialIcon url="https://www.linkedin.com/in/sergei-poliakov-6b2b52213/"
+														style={{height: 30, width: 30}}/></li>
+						<li><SocialIcon url="https://github.com/sergei-p"
+														style={{height: 30, width: 30}}/></li>
+					</ul>
+				</div>
+				<div className="navLinks">
+					<ul>
+						<li><a href="">About Me</a></li>
+						<li><a href="">Projects</a></li>
+						<li><a href="">Contact</a></li>
+					</ul>
+				</div>
+			{/* </div> */}
+		</nav>
+	
+	);
+
+}
+
+export default NavBar;
