d802d246b9d9b834a33623f7b7d6ba63c8881470
sergei-p <sergeidev2494@gmail.com>
2022-02-11 10:56:33 -0800

    Change Copyright year from 2021 to 2022

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return(
     <footer>
-      <p>Copyright &copy; Sergei Poliakov 2021</p>
+      <p>Copyright &copy; Sergei Poliakov 2022</p>
     </footer>
   )
 }
